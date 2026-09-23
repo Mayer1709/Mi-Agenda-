894f33ee1334fd5bcba11a196862b95bec65d9e3
Mayer1709 <Torresmayerly650@gmail.com>
2026-09-23 08:47:14 -0400

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,5 @@
+const CACHE="mi-agenda-pwa-v1";
+const ASSETS=["./Mi_Agenda_Dinamica.html","./manifest.webmanifest","./icon-192.png","./icon-512.png"];
+self.addEventListener("install",e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)).then(()=>self.skipWaiting())));
+self.addEventListener("activate",e=>e.waitUntil(self.clients.claim()));
+self.addEventListener("fetch",e=>e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request).then(x=>{const copy=x.clone();caches.open(CACHE).then(c=>c.put(e.request,copy));return x}).catch(()=>caches.match("./Mi_Agenda_Dinamica.html")))));
