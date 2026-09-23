bfba0049009136d85a10e1bdc53a3282e024db6b
Mayer1709 <Torresmayerly650@gmail.com>
2026-09-23 09:59:37 -0400

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,5 +1,4 @@
-const CACHE="mi-agenda-pwa-v1";
-const ASSETS=["./Mi_Agenda_Dinamica.html","./manifest.webmanifest","./icon-192.png","./icon-512.png"];
-self.addEventListener("install",e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)).then(()=>self.skipWaiting())));
-self.addEventListener("activate",e=>e.waitUntil(self.clients.claim()));
-self.addEventListener("fetch",e=>e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request).then(x=>{const copy=x.clone();caches.open(CACHE).then(c=>c.put(e.request,copy));return x}).catch(()=>caches.match("./Mi_Agenda_Dinamica.html")))));
+const CACHE='mi-agenda-assistant-v2';
+self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(['./','./index.html','./manifest.webmanifest']))));
+self.addEventListener('activate',e=>e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k))))));
+self.addEventListener('fetch',e=>e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request))));
